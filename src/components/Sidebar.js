2b83bb3d7db5e8b517c1fd1da25a57b4162d5203
Дмитрий Метельков <d.metelkov@mail.ru>
2018-08-20 21:46:33 +0400

    Добавил переключение state для остановок

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,61 +2,78 @@ import React from 'react'
 
 class Sidebar extends React.Component {
   state = {
-    currencyData: [
-      {
-        id: 1,
-        class: 'sidebar__currency-label',
-        text: 'RUB',
-        value: 'rubles'
-      },
-      {
-        id: 2,
-        class: 'sidebar__currency-label',
-        text: 'USD',
-        value: 'dollars'
-      },
-      {
-        id: 3,
-        class: 'sidebar__currency-label',
-        text: 'EUR',
-        value: 'euro'
-      }
-    ],
-    transferData: [
-      {
-        id: 1,
-        class: 'sidebar__transfer-label',
-        text: 'Все',
-        value: 'all'
-      },
-      {
-        id: 2,
-        class: 'sidebar__transfer-label',
-        text: 'Без пересадок',
-        value: 'none'
-      },
-      {
-        id: 3,
-        class: 'sidebar__transfer-label',
-        text: '1 пересадка',
-        value: 'one'
-      },
-      {
-        id: 4,
-        class: 'sidebar__transfer-label',
-        text: '2 пересадки',
-        value: 'two'
-      },
-      {
-        id: 5,
-        class: 'sidebar__transfer-label',
-        text: '3 пересадки',
-        value: 'three'
-      }
-    ]
+    currency: 'rub',
+    all: false,
+    none: false,
+    one: false,
+    two: false,
+    three: false
   };
 
-  createRadioItem = (item, name) => {
+  toggleCheckbox = (e) => {
+    const stopName = e.target.name;
+    this.setState(
+      {
+        [`${stopName}`]: !this.state[`${stopName}`]
+      }
+    );
+  };
+
+  currencyData = [
+    {
+      id: 1,
+      class: 'sidebar__currency-label',
+      text: 'RUB',
+      value: 'rubles'
+    },
+    {
+      id: 2,
+      class: 'sidebar__currency-label',
+      text: 'USD',
+      value: 'dollars'
+    },
+    {
+      id: 3,
+      class: 'sidebar__currency-label',
+      text: 'EUR',
+      value: 'euro'
+    }
+  ];
+
+  stopsData = [
+    {
+      id: 1,
+      class: 'sidebar__stops-label',
+      text: 'Все',
+      value: 'all'
+    },
+    {
+      id: 2,
+      class: 'sidebar__stops-label',
+      text: 'Без пересадок',
+      value: 'none'
+    },
+    {
+      id: 3,
+      class: 'sidebar__stops-label',
+      text: '1 пересадка',
+      value: 'one'
+    },
+    {
+      id: 4,
+      class: 'sidebar__stops-label',
+      text: '2 пересадки',
+      value: 'two'
+    },
+    {
+      id: 5,
+      class: 'sidebar__stops-label',
+      text: '3 пересадки',
+      value: 'three'
+    }
+  ];
+
+  createRadioItem(item, name) {
     return (
       <label key={item.id} className={item.class}>
         <input
@@ -70,27 +87,28 @@ class Sidebar extends React.Component {
     )
   };
 
-  renderRadioList = (data, name) => {
+  renderRadioList(data, name) {
     let currencyList = data.map((item) => {
       return this.createRadioItem(item, name);
     });
     return currencyList
   };
 
-  createCheckboxItem = (item) => {
+  createCheckboxItem(item) {
     return (
       <label key={item.id} className={item.class}>
         <input
           className='visually-hidden'
           type="checkbox"
           name={item.value}
+          onChange={this.toggleCheckbox}
         />
         {item.text}
       </label>
     )
   };
 
-  renderCheckboxList = (data) => {
+  renderCheckboxList(data) {
     let currencyList = data.map((item) => {
       return this.createCheckboxItem(item);
     });
@@ -102,11 +120,11 @@ class Sidebar extends React.Component {
       <form className='sidebar'>
         <fieldset className='sidebar__fieldset'>
           <legend className='sidebar__legend'>Валюта</legend>
-          {this.renderRadioList(this.state.currencyData, 'currency')};
+          {this.renderRadioList(this.currencyData, 'currency')};
         </fieldset>
         <fieldset className='sidebar__fieldset'>
           <legend className='sidebar__legend'>Количество пересадок</legend>
-          {this.renderCheckboxList(this.state.transferData)};
+          {this.renderCheckboxList(this.stopsData)};
         </fieldset>
       </form>
     );
