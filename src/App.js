e9bb6d4e42173ec5246b4012a5303193f6c284a1
Дмитрий Метельков <d.metelkov@mail.ru>
2018-08-23 17:45:31 +0400

    Вернул скачивание билетов в state app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ import Results from './components/Results'
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    tickets: null,
+    isLoading: false,
+  };
+
+  componentDidMount() {
+    this.setState({isLoading: true});
+    fetch('http://localhost:3000/data/tickets.json')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      setTimeout(() => {
+        this.setState({isLoading: false, tickets: data})
+      }, 1000)
+    })
+  }
 
   render() {
+    const data = this.state.tickets;
     return (
       <div className='app'>
         <Header/>
-        <Results/>
+        <Results isLoading={this.state.isLoading} data={data}/>
       </div>
     );
   }
