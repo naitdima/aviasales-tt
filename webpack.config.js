3793cadaf2aaa435dae0dcc176efdb9007946ccf
Дмитрий Метельков <d.metelkov@mail.ru>
2018-08-31 11:16:00 +0400

    Добавил fetch polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: ['whatwg-fetch', './src/index.js'],
   output:{
     path: path.resolve(__dirname, 'build'),
     filename: 'index.[hash].js'
