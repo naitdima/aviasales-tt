d2c446bbdffde7a305122cb55ea6468617374613
Дмитрий Метельков <d.metelkov@mail.ru>
2018-08-31 09:21:50 +0400

    Доработка конфига webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,32 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackMd5Hash = require('webpack-md5-hash');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: { main: './src/index.js' },
   output:{
-    path: path.resolve(__dirname, './build'),
-    publicPath: '/build/',
-    filename: "index.js"
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.[hash].js'
   },
   module:{
     rules:[
       {
         test: /\.js?$/,
         exclude: /(node_modules)/,
-        loader: "babel-loader",
-        options:{
-          presets:["env", "react"]
-        }
+        loader: "babel-loader"
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-
+        test: /\.(gif|png|jpe?g|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: "[name].[ext]",
+            outputPath: 'img/'
+          }
+        }, {
           loader: 'image-webpack-loader',
           options: {
             mozjpeg: {
@@ -27,7 +34,27 @@ module.exports = {
               quality: 70
             }
           }
+        },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: 'src/img', to: 'img' }, { from: 'src/data', to: 'data' }]),
+    new CleanWebpackPlugin('build', {} ),
+    new MiniCssExtractPlugin({
+      filename: 'index.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
 };
